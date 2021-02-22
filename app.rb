09af6c65968b55398c139dd6d93ac60873e1fec1
lukevangorder <lukevangorder@gmail.com>
2021-02-22 13:12:07 -0500

    commit tree 0be1c9030b6e602c136cbfeacf5aa2741117223b

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    
+    get '/name' do
+        return 'My name is '
+    end
+
+    get '/hometown' do
+        return 'My hometown is '
+    end
+
+    get '/favorite-song' do
+        return 'My favorite song is '
+    end
+    
 end
